f101168d910b5673d73137940ac31f2d6f210c98
xw <xwdaysme@gmail.com>
2021-06-22 20:53:52 +0800

    Update getSize.js

--- tool/getSize.js
+++ tool/getSize.js
@@ -1,13 +1,29 @@
 const body = document.body
-const height = getComputedStyle(body).height
-window.parent.postMessage({ action: `resize`, data: height }, `*`)
-const observer = new MutationObserver(() => {
-  const height = getComputedStyle(body).height
-  window.parent.postMessage({ action: `resize`, data: height }, `*`)
-})
+
+sendSize() // 初始化时
+addLoadEvent(sendSize) // 页面内容加载完成时
+const observer = new MutationObserver(sendSize) // 当结点变更时
 observer.disconnect()
 observer.observe(body, {
   attributes: true,
   childList: true,
   subtree: true,
 })
+
+function addLoadEvent(func) {
+  var oldonload = window.onload;
+  if (typeof window.onload != 'function') {
+    window.onload = func;
+  } else {
+    window.onload = function () {
+      oldonload();
+      func();
+    }
+  }
+}
+
+function sendSize() {
+  const height = getComputedStyle(body).height
+  console.log(`height`, height)
+  window.parent.postMessage({ action: `resize`, data: height }, `*`)
+}
